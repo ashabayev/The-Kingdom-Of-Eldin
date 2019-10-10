93634e22b8413f37be8f325d0dcc3a1e56a32f4a
Simran Desai <sdesai51@illinois.edu>
2019-10-10 15:27:13 -0500

    Adding untracked files

---
+++ The Kingdom Of Eldin/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public Image damageImage;
+    public AudioClip deathClip;
+    public float flashSpeed = 5f;
+    public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+
+    Animator anim;
+    AudioSource playerAudio;
+    PlayerController2D playerControl;
+    bool isDead;
+    bool damaged;
+
+   /**  private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        playerControl = GetComponent<PlayerController2D>();
+        currentHealth = startingHealth;
+    }
+    */
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        playerControl = GetComponent<PlayerController2D>();
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (damaged)
+        {
+            damageImage.color = flashColor;
+        } else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
+    }
+
+    public void TakeDamage (int amount)
+    {
+        damaged = true;
+        currentHealth -= amount;
+        healthSlider.value = currentHealth;
+        playerAudio.Play();
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+        anim.SetTrigger("Die");
+        playerControl.enabled = false;
+    }
+}
