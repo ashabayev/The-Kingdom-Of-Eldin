16b9e2d3013220c08bba7553466a0336582b4e12
Simran Desai <42985598+sdesai51@users.noreply.github.com>
2019-11-18 14:17:57 -0600

    Revert "working on parallax background scrolling."
    This reverts commit b742da692518c3046cfeb68bc95e139c6ee21228.

---
+++ The Kingdom Of Eldin/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public int minHealth = 0;
+    public Slider healthSlider;
+    public Image damageImage;
+    public AudioClip deathClip;
+    public float flashSpeed = 5f;
+    public Color flashColor = new Color(1f, 0f, 0f, 0.1f);
+    public Vector3 startPosition;
+
+    Animator anim;
+    AudioSource playerAudio;
+    PlayerController2D playerControl;
+    bool isDead;
+    bool damaged;
+
+    private void Awake()
+    {
+        startPosition = transform.position;
+        print(startPosition);
+    }
+
+    void Death()
+    {
+        isDead = true;
+        transform.position = startPosition;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        playerControl = GetComponent<PlayerController2D>();
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthSlider.value = currentHealth;
+        if (damaged)
+        {
+            damageImage.color = flashColor;
+        } else
+        {
+            damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        }
+        damaged = false;
+    }
+
+    public void TakeDamage (int amount)
+    {
+        bool dashInvincibility = PlayerController2D.isDashing;
+        bool blockInvincibility = PlayerController2D.isBlocking;
+        if (dashInvincibility == true | blockInvincibility == true)
+        {
+            return;
+        }
+
+        damaged = true;
+        currentHealth -= amount;
+        healthSlider.value = currentHealth;
+        playerAudio.Play();
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+}
