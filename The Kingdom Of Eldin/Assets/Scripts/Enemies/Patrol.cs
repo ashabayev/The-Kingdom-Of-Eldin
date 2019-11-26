235d9be9d510c5e8f82cd01430621016512adea5
Anna <annashabayev@gmail.com>
2019-11-26 01:50:19 -0600

    fixed enemy patrol script

--- The Kingdom Of Eldin/Assets/Scripts/Enemies/Patrol.cs
+++ The Kingdom Of Eldin/Assets/Scripts/Enemies/Patrol.cs
@@ -18,7 +18,7 @@ public class Patrol : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 2f);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, .5f);
         if (groundInfo.collider == false)
         {
             if (movingRight == true)
