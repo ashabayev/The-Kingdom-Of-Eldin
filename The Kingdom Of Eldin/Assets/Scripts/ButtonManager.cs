5b249415465b44dea2cd494b376018faa9e61087
Simran Desai <sdesai51@illinois.edu>
2019-11-25 23:11:24 -0600

    test

---
+++ The Kingdom Of Eldin/Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour
+{
+    public Button PlayButton;
+    public string newGameSceneName;
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(newGameSceneName);
+    }
+}
